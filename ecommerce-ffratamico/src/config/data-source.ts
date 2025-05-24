c59c8cfcbed842df5c71cfbea40d54071378d2fb
FFratamico <faustofratamico1@gmail.com>
2025-05-24 14:42:07 -0300

    modif de data-source para uso de entorno local

--- ecommerce-ffratamico/src/config/data-source.ts
+++ ecommerce-ffratamico/src/config/data-source.ts
@@ -7,15 +7,15 @@ const isDocker = process.env.NODE_ENV === 'docker';
 
 export const AppDataSource: DataSourceOptions = ({
     type: 'postgres',
-    host: isDocker ? process.env.DB_HOST_DOCKER : process.env.DB_HOST,
-    port: parseInt(isDocker ? process.env.DB_PORT_DOCKER : process.env.DB_PORT!, 10),
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT!, 10),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: process.env.NODE_ENV === 'docker' ? false : false,
+    synchronize: true,
     entities: ['dist/**/*.entity{.js,.ts}'], // busca automaticamente todas las entidades del proyecto.
     migrations: ['dist/migrations/*{.ts,.js}'],
-    ssl: isDocker ? { rejectUnauthorized: false } : false,
+    ssl: false,
 });
 
 export const postgresDataSourceConfig = registerAs(
